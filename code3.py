ee140a8dfa5bddf65e6e91cb1d71a533be0a65be
Michal Tymejczyk <michaltymejczyk@gmail.com>
2018-12-23 00:37:17 +0100

    Added second func

--- code3.py
+++ code3.py
@@ -1,5 +1,6 @@
 def main():
-    print(sprytne(2,13))
+    # print(sprytne(2,13))  
+    czyPalindrom()
 
 def sprytne(x,y):
     if y == 1: 
@@ -14,6 +15,20 @@ def sprytne(x,y):
     else:
         #nieparzyste
         return 2*sprytne(x, int(y/2))*sprytne(x, int(y/2))
+
+def czyPalindrom():
+    napis = input("Podaj napis: ")
+    napis = napis.lower()   
+    flag = True
+    for i in range(0, int(len(napis) / 2)):
+        print(i)
+        if napis[i] != napis[-(i + 1)]:
+            flag = False
+            break
+    if flag == True:
+        print(flag)
+    else: 
+        print(flag)
 
 if __name__ == "__main__":
     main()
