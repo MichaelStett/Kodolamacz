b2083bd03bbe2b341fb54e124a3f50a959d3d452
Michal Tymejczyk <michaltymejczyk@gmail.com>
2018-12-23 02:40:18 +0100

    Finished code3.py

--- code3.py
+++ code3.py
@@ -1,11 +1,22 @@
 def main():
-    # print(sprytne(2,13))  
-    # czyPalindrom()
-    # x = input("Podaj napis1: ")
-    # y = input("Podaj napis2: ")
-    # print(czyAnagram(x, y))
-    Lista = [1,2,3,4]
-    print(moda(Lista))
+    #Potegi
+    print("sprytne(2,7) = " + str(sprytne(2,7)))
+    print("sprytne(2,10) = " + str(sprytne(2,10)))
+    #Palindrom
+    print("czyPalindrom(\"kajak\") = ", end="")
+    czyPalindrom("kajak")
+    print("czyPalindrom(\"abba\") = ", end="")
+    czyPalindrom("abba")
+    print("czyPalindrom(\"słowo\") = ", end="")
+    czyPalindrom("słowo")
+    #Anagram
+    print("czyAnagram(\"kappa\", \"papak\") = " + str(czyAnagram("kappa", "papak")))
+    print("czyAnagram(\"kappa\", \"papaj\") = " + str(czyAnagram("kappa", "papaj")))
+    #Moda
+    Lista1 = [1,1,3,5,2,8,2]
+    print("moda(" + str(Lista1) + ") = " +  str(moda(Lista1)))
+    Lista2 = [2,2,2,3,4,4,4]
+    print("moda(" + str(Lista2) + ") = " +  str(moda(Lista2)))
 
 def sprytne(x,y):
     if y == 1: 
@@ -21,12 +32,9 @@ def sprytne(x,y):
         #nieparzyste
         return 2*sprytne(x, int(y/2))*sprytne(x, int(y/2))
 
-def czyPalindrom():
-    napis = input("Podaj napis: ")
-    napis = napis.lower()   
+def czyPalindrom(napis):  
     flag = True
     for i in range(0, int(len(napis) / 2)):
-        print(i)
         if napis[i] != napis[-(i + 1)]:
             flag = False
             break
@@ -52,14 +60,14 @@ def czyAnagram(napis1, napis2):
                 if chr(ord('a') + i) == napis1[j]:
                     alfabet[chr(ord('a') + i)] += 1
                 if chr(ord('a') + i) == napis2[j]:
-                    alfabet[chr(ord('a') + i)] -= 1        
+                    alfabet[chr(ord('a') + i)] -= 1  
         return all(value == 0 for value in alfabet.values())
     else: 
         return False
 
 def moda(liczby):
     hold = {}
-
+    
     for i in range(0, len(liczby)):
         if liczby[i] in hold:
             hold[liczby[i]] =+ 1
