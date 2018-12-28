1a628368c2dd0d0ef6d0f651bfdab9a009a94a92
Michal Tymejczyk <michaltymejczyk@gmail.com>
2018-12-28 09:53:55 +0100

    Added second func

--- code4.py
+++ code4.py
@@ -41,7 +41,28 @@ class FunkcjaKwadratowa:
                     print(f"dwa miejsca zerowe: {x1} oraz {x2}.")
                     # print(f"Sprawdzenie: {self.wartoscDla(x1)} oraz {self.wartoscDla(x2)}")
 
+class Zespolona:
+    def __init__(self, re, im):
+        self.re = re
+        self.im = im
 
+    def print(self):
+        if self.im < 0:
+            print(f"{self.re}{self.im}i")
+        else:
+            print(f"{self.re}+{self.im}i")
+
+    def modul(self): # zaookrąglona do 4 miejsc po przecinku
+        print(round(math.sqrt(self.re**2 + self.im**2), 4))  
+
+    @staticmethod
+    def dodaj(z1, z2):
+        return Zespolona(z1.re + z2.re, z1.im + z2.im)
+
+    @staticmethod
+    def mnoz(z1, z2): # (a+bi)*(c+di) = ac-bd + (bc+ad)i
+        return Zespolona(z1.re*z2.re-z1.im*z2.im, z1.im*z2.re + z1.re*z2.im)
+    
 def main():
     #region FunkcjaKwadratowa 
     FunkcjaKwadratowa(1, 6, 5).Rozwiaz() # delta dodatnia ( 4 )
@@ -51,6 +72,19 @@ def main():
     FunkcjaKwadratowa(0, 1 ,0).Rozwiaz() # a == 0 and c == 0
     FunkcjaKwadratowa(0, 0, 1).Rozwiaz() # a == 0 and b == 0
     FunkcjaKwadratowa(0, 0 ,0).Rozwiaz() # a == 0 and b == 0 and c == 0
+    #endregion
+    
+    print()
+
+    #region Zespolona
+    Z1 = Zespolona(3,4)
+    Z1.print()
+    Z1.modul()
+    Z2 = Zespolona(2,-1)
+    Z2.print()
+    Zespolona.dodaj(Z1, Z2).print()
+    Zespolona.mnoz(Z1, Z2).print()
+    Z1,Z2 = None,None    
     #endregion
 
 if __name__ == "__main__":
